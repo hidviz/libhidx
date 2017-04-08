ee6c81467f4115bb9494335d629de7dc8baa2fc8
Ondřej Budai <ondrej@budai.cz>
2017-04-08 17:06:51 +0200

    Extract libhidx from hidviz

--- src/hidviz/DeviceSelectionListItem.cc
+++
@@ -1,6 +0,0 @@
-#include "DeviceSelectionListItem.hh"
-
-hidviz::DeviceSelectionListItem::DeviceSelectionListItem(const QString& text,
-                                                         libhidx::Interface& interface) : QListWidgetItem{text}, m_interface{interface} {
-
-}
