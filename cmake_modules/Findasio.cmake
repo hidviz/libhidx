3a535b007549f71ca09348f6676bdd6e1bfa0234
Ondřej Budai <ondrej@budai.cz>
2017-05-01 17:07:55 +0200

    Change CMake to always use custom asio

--- cmake_modules/Findasio.cmake
+++ cmake_modules/Findasio.cmake
@@ -1,28 +1,24 @@
+if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/asio.hpp")
+    find_package(Git)
 
-find_path( ASIO_INCLUDE_DIR asio.hpp HINTS "${CMAKE_CURRENT_SOURCE_DIR}/asio/asio/include" /usr/include /usr/local/include )
+    if(NOT GIT_FOUND)
+        message(FATAL_ERROR "Git not found")
+    endif()
 
-if( ASIO_INCLUDE_DIR )
-    set( ASIO_FOUND TRUE )
-    set( ASIO_INCLUDE_DIRS ${ASIO_INCLUDE_DIR} )
-endif( ASIO_INCLUDE_DIR )
+    execute_process(
+            COMMAND "${GIT_EXECUTABLE}" clone --branch asio-1-10-8 --depth=1 https://github.com/chriskohlhoff/asio.git
+            WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" RESULT_VARIABLE RESULT
+    )
 
-if( ASIO_FOUND )
-    if( NOT ASIO_FIND_QUIETLY )
-        message( STATUS "Found ASIO: ${ASIO_INCLUDE_DIR}" )
-    endif( NOT ASIO_FIND_QUIETLY )
+    if(NOT RESULT EQUAL 0)
+        message(FATAL_ERROR "Cannot clone asio repository")
+    endif()
+endif()
 
-    add_library(asio INTERFACE)
-    target_include_directories(asio INTERFACE "${ASIO_INCLUDE_DIR}")
-    target_compile_definitions(asio INTERFACE ASIO_STANDALONE)
+add_library(asio INTERFACE)
+target_include_directories(asio INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/")
+target_compile_definitions(asio INTERFACE ASIO_STANDALONE)
 
-    if(WIN32)
-        target_link_libraries(asio INTERFACE ws2_32)
-    endif(WIN32)
-
-else( ASIO_FOUND )
-
-    if( ASIO_FIND_REQUIRED )
-        message( FATAL_ERROR "Could not find Asio-lib" )
-    endif( ASIO_FIND_REQUIRED )
-
-endif( ASIO_FOUND )
+if(WIN32)
+    target_link_libraries(asio INTERFACE ws2_32)
+endif(WIN32)
--- getasio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -d asio ];then
-exit 0
-fi
-git clone --branch asio-1-10-8 --depth=1 https://github.com/chriskohlhoff/asio.git
-
-
