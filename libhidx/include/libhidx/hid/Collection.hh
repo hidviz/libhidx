ea764399a06791d4ec3d9c7c71232b15826d0b03
Ondřej Budai <ondrej@budai.cz>
2017-05-01 14:10:19 +0200

    Add better API for collections

--- libhidx/include/libhidx/hid/Collection.hh
+++ libhidx/include/libhidx/hid/Collection.hh
@@ -4,12 +4,18 @@
 #include "Item.hh"
 
 namespace libhidx {
+    class Parser;
 namespace hid {
     class Collection : public Item {
-
+    friend class libhidx::Parser;
     public:
         explicit Collection(Item* parent = nullptr) : Item{parent} {}
+        auto getType(){return m_type;}
+        std::string getTypeStr();
+        auto getUsage(){return m_usage;}
+        std::string getUsageStr();
 
+    private:
         uint32_t m_type = 0;
         unsigned m_usage = 0;
     };
--- libhidx/src/hid/Collection.cc
+++ libhidx/src/hid/Collection.cc
@@ -1,6 +1,23 @@
 #include <libhidx/hid/Collection.hh>
+#include <libhidx/Usages.hh>
 
 namespace libhidx {
 namespace hid {
+    std::string Collection::getTypeStr() {
+        static const char* types[] = {
+            "Physical", "Application", "Logical", "Report", "Named Array",
+            "Usage Switch", "Usage modifier"
+        };
+
+        if(m_type >= (sizeof(types) / sizeof(*types))){
+            return "Unknown";
+        }
+
+        return types[m_type];
+    }
+
+    std::string hid::Collection::getUsageStr() {
+        return getHidUsageText(m_usage);
+    }
 }
 }
