da37738155f315748dbcf0aa75af4769a71accb2
Ondřej Budai <obudai@redhat.com>
2019-12-02 07:17:54 +0100

    use system asio when available
    Asio is cloned if system one isn't available.

--- cmake_modules/Findasio.cmake
+++ cmake_modules/Findasio.cmake
@@ -1,22 +1,27 @@
-if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/asio.hpp")
-    find_package(Git)
+add_library(asio INTERFACE)
 
-    if(NOT GIT_FOUND)
-        message(FATAL_ERROR "Git not found")
+if(EXISTS "/usr/include/asio.hpp")
+    target_include_directories(asio INTERFACE "/usr/include/asio")
+else()
+    if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/asio.hpp")
+        find_package(Git)
+
+        if(NOT GIT_FOUND)
+            message(FATAL_ERROR "Git not found")
+        endif()
+
+        execute_process(
+                COMMAND "${GIT_EXECUTABLE}" clone --branch asio-1-14-0 --depth=1 https://github.com/chriskohlhoff/asio.git
+                WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" RESULT_VARIABLE RESULT
+        )
+
+        if(NOT RESULT EQUAL 0)
+            message(FATAL_ERROR "Cannot clone asio repository")
+        endif()
     endif()
-
-    execute_process(
-            COMMAND "${GIT_EXECUTABLE}" clone --branch asio-1-10-8 --depth=1 https://github.com/chriskohlhoff/asio.git
-            WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" RESULT_VARIABLE RESULT
-    )
-
-    if(NOT RESULT EQUAL 0)
-        message(FATAL_ERROR "Cannot clone asio repository")
-    endif()
+    target_include_directories(asio INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/")
 endif()
 
-add_library(asio INTERFACE)
-target_include_directories(asio INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/asio/asio/include/")
 target_compile_definitions(asio INTERFACE ASIO_STANDALONE)
 
 if(WIN32)
